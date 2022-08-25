8b78f6f1c6bd2173c4c1ade5a11da6f50b9f7e06
bizhermit <rsenda@bizhermit.com>
2022-08-25 18:20:45 +0900

    save layout

--- dev/src/components/layout-selector.tsx
+++ dev/src/components/layout-selector.tsx
@@ -3,22 +3,28 @@ import Row from "../../react-addon/dist/elements/row";
 import { LayoutColorRadioButtons, LayoutColorSelectBox } from "../../react-addon/dist/components/layout-color";
 import { LayoutDesignRadioButtons, LayoutDesignSelectBox } from "../../react-addon/dist/components/layout-design";
 import { useLayout } from "../../react-addon/dist/styles/layout-provider";
+import useElectron from "../hooks/electron";
 
 const LayoutBox: FC = () => {
   const layout = useLayout();
+  const electron = useElectron();
   return (
     <Row $fill>
       <LayoutColorRadioButtons $unset style={{ marginRight: 0 }} $changed={(v) => {
         layout.setColor(v.after);
+        electron?.setLayoutColor(v.after);
       }} />
       <LayoutColorSelectBox $unset $notInputText style={{ marginRight: 0, width: 100 }} $changed={(v) => {
         layout.setColor(v.after);
+        electron?.setLayoutColor(v.after);
       }} />
       <LayoutDesignRadioButtons $unset style={{ marginRight: 0 }} $changed={(v) => {
         layout.setDesign(v.after);
+        electron?.setLayoutDesign(v.after);
       }} />
       <LayoutDesignSelectBox $unset $notInputText style={{ width: 150 }} $changed={(v) => {
         layout.setDesign(v.after);
+        electron?.setLayoutDesign(v.after);
       }} />
     </Row>
   );
