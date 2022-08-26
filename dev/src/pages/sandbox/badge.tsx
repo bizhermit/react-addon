21d6655c20986cfcf241df9266edd67ce1c55f4f
bizhermit <rsenda@bizhermit.com>
2022-08-27 00:52:02 +0900

    update

--- dev/src/pages/sandbox/badge.tsx
+++ dev/src/pages/sandbox/badge.tsx
@@ -3,6 +3,7 @@ import Badge from "../../../react-addon/dist/elements/badge";
 import Button from "../../../react-addon/dist/elements/button";
 import Caption from "../../../react-addon/dist/elements/caption";
 import FlexBox from "../../../react-addon/dist/elements/flex-box";
+import Icon from "../../../react-addon/dist/elements/icon";
 import Label from "../../../react-addon/dist/elements/label";
 import Row from "../../../react-addon/dist/elements/row";
 import { colorIterator } from "../../../react-addon/dist/styles/css-var";
@@ -12,16 +13,10 @@ const Page: NextPage = () => {
     <FlexBox $fto="fy" $scroll $padding>
       <Label>Positoin</Label>
       <FlexBox $fto="x" $row style={{ gap: 20 }}>
-        <Badge $content="1" $position="left">
-          <Button $icon="message" />
-        </Badge>
         <Badge $content="1" $position="left-top">
           <Button $icon="message" />
         </Badge>
         <Badge $content="1" $position="left-bottom">
-          <Button $icon="message" />
-        </Badge>
-        <Badge $content="1" $position="right">
           <Button $icon="message" />
         </Badge>
         <Badge $content="1" $position="right-top">
@@ -34,37 +29,63 @@ const Page: NextPage = () => {
       <Label>Size</Label>
       <FlexBox $fto="x" $row style={{ gap: 20 }}>
         <Badge $content="1" $size="xs">
-          <Button $icon="message" />
+          <Button $icon="message" $size="xs" />
         </Badge>
         <Badge $content="1" $size="s">
-          <Button $icon="message" />
+          <Button $icon="message" $size="s" />
         </Badge>
         <Badge $content="1" $size="m">
-          <Button $icon="message" />
+          <Button $icon="message" $size="m" />
         </Badge>
         <Badge $content="1" $size="l">
-          <Button $icon="message" />
+          <Button $icon="message" $size="l" />
         </Badge>
         <Badge $content="1" $size="xl">
-          <Button $icon="message" />
+          <Button $icon="message" $size="xl" />
+        </Badge>
+      </FlexBox>
+      <FlexBox $fto="x" $row style={{ gap: 20 }}>
+        <Badge $content={<Icon $image="c-check" $color="primary" />} $size="xs">
+          <Button $icon="message" $size="xs" />
+        </Badge>
+        <Badge $content={<Icon $image="c-check" $color="primary" />} $size="s">
+          <Button $icon="message" $size="s" />
+        </Badge>
+        <Badge $content={<Icon $image="c-check" $color="primary" />} $size="m">
+          <Button $icon="message" $size="m" />
+        </Badge>
+        <Badge $content={<Icon $image="c-check" $color="primary" />} $size="l">
+          <Button $icon="message" $size="l" />
+        </Badge>
+        <Badge $content={<Icon $image="c-check" $color="primary" />} $size="xl">
+          <Button $icon="message" $size="xl" />
         </Badge>
       </FlexBox>
       <Label>Color</Label>
       <FlexBox $fto="x">
         {colorIterator(c => {
           return (
-            <Caption $label={c} $width={100} $color={c}>
+            <Caption key={c} $label={c} $width={100} $color={c}>
               <Row key={c} style={{ gap: 20 }}>
-                <Badge $content="1" $color={c} $shape="circle">
+                <Badge $content={<Icon $color="danger" $image="error" />} $color={c} $colorType="base" $shape="circle">
                   <Button $icon="message" />
                 </Badge>
-                <Badge $content="1" $color={c} $fill $shape="circle">
+                <Badge $content="1" $color={c} $colorType="base" $shape="circle">
                   <Button $icon="message" />
                 </Badge>
-                <Badge $content="1" $color={c} $shape="square">
+                <Badge $content="1" $color={c} $colorType="head" $shape="circle">
                   <Button $icon="message" />
                 </Badge>
-                <Badge $content="1" $color={c} $fill $shape="square">
+                <Badge $content="1" $color={c} $colorType="nav" $shape="circle">
+                  <Button $icon="message" />
+                </Badge>
+                <Badge $content="1" $color={c} $colorType="base" $shape="square">
+                  <Button $icon="message" />
+                </Badge>
+                <Badge $content="1" $color={c} $colorType="head" $shape="square">
+                  <Button $icon="message" />
+                </Badge>
+                <Badge $content="1" $color={c} $colorType="nav" $shape="square">
                   <Button $icon="message" />
                 </Badge>
               </Row>
--- src/dist/elements/badge.tsx
+++ src/dist/elements/badge.tsx
@@ -1,13 +1,15 @@
 import React, { FC, ReactNode } from "react";
-import { Color, colorIterator, Size, sizeIterator } from "../styles/css-var";
+import CssVar, { Color, colorIterator, CssPV, Size, sizeIterator, varFontSize } from "../styles/css-var";
 import JsxStyle from "../styles/jsx-style";
 import { attributesWithoutChildren } from "../utils/attributes";
+import Label from "./label";
 
 const cn = "bh-bde";
 
 const Badge: FC<{
-  $position?: "left" | "right" | "left-top" | "right-top" | "left-bottom" | "right-bottom";
+  $position?: "left-top" | "right-top" | "left-bottom" | "right-bottom";
   $color?: Color;
+  $colorType?: "base" | "head" | "nav";
   $fill?: boolean;
   $shape?: "circle" | "square";
   $size?: Size;
@@ -22,10 +24,14 @@ const Badge: FC<{
         className={`${cn}-main`}
         data-pos={attrs.$position || "right-top"}
         data-color={attrs.$color}
+        data-colortype={attrs.$colorType || "base"}
         data-fill={attrs.$fill}
         data-size={attrs.$size ?? "m"}
         data-shape={attrs.$shape || "circle"}
-      >{attrs.$content}</div>
+      >{
+        typeof attrs.$content === "string" || typeof attrs.$content === "number" ?
+          <Label>{attrs.$content}</Label> : attrs.$content
+      }</div>
       {BadgeStyle}
     </div>
   );
@@ -41,34 +47,78 @@ const BadgeStyle = <JsxStyle id={cn}>{() => `
   box-sizing: border-box;
   position: absolute;
   z-index: 999;
-}
-.${cn}-main[data-pos="left"] {
-
-}
-.${cn}-main[data-pos="right"] {
-
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+  opacity: 0.9;
 }
 .${cn}-main[data-pos="left-top"] {
-
+  top: calc(${CssVar.pdy} * -0.5);
+  left: calc(${CssVar.pdx} * -0.5);
 }
 .${cn}-main[data-pos="right-top"] {
-
+  top: calc(${CssVar.pdy} * -0.5);
+  right: calc(${CssVar.pdx} * -0.5);
 }
 .${cn}-main[data-pos="left-bottom"] {
-
+  bottom: calc(${CssVar.pdy} * -0.5);
+  left: calc(${CssVar.pdx} * -0.5);
 }
 .${cn}-main[data-pos="right-bottom"] {
-
+  bottom: calc(${CssVar.pdy} * -0.5);
+  right: calc(${CssVar.pdx} * -0.5);
 }
-${sizeIterator(cn, {
-xs: ``,
-s: ``,
-m: ``,
-l: ``,
-xl: ``,
+.${cn}-main[data-shape="circle"] {
+  border-radius: 9999px;
+}
+.${cn}-main[data-shape="square"] {
+  border-radius: ${CssVar.bdr};
+}
+${sizeIterator(`${cn}-main`, {
+xs: `
+height: calc(${CssVar.size} * 0.4);
+width: calc(${CssVar.size} * 0.4);
+font-size: 1.0rem;
+${varFontSize}: 1.1rem;
+`,
+s: `
+height: calc(${CssVar.size} * 0.5);
+width: calc(${CssVar.size} * 0.5);
+font-size: 1.1rem;
+${varFontSize}: 1.25rem;
+`,
+m: `
+height: calc(${CssVar.size} * 0.6);
+width: calc(${CssVar.size} * 0.6);
+font-size: 1.2rem;
+${varFontSize}: 1.4rem;
+`,
+l: `
+height: calc(${CssVar.size} * 0.7);
+width: calc(${CssVar.size} * 0.7);
+font-size: 1.3rem;
+${varFontSize}: 1.6rem;
+`,
+xl: `
+height: calc(${CssVar.size} * 0.8);
+width: calc(${CssVar.size} * 0.8);
+font-size: 1.6rem;
+${varFontSize}: 1.8rem;
+`,
 })}
 ${colorIterator((_c, v, s) => `
-.${cn}-main${s} {
+.${cn}-main${s}[data-colortype="base"] {
+  background: ${v.bgc};
+  color: ${v.fc};
+  border: 1px solid ${v.bdc};
+}
+.${cn}-main${s}[data-colortype="head"] {
+  background: ${v.head.bgc};
+  color: ${v.head.fc};
+  border: 1px solid ${v.head.bdc};
+}
+.${cn}-main${s}[data-colortype="nav"] {
   background: ${v.nav.bgc};
   color: ${v.nav.fc};
 }
